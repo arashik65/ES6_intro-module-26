671ecf50d1e8fceee8c8871ff132a7610b3d58ab
arashik65 <mdashikur6398+@gmail.com>
2026-03-16 02:55:40 +0600

    object introduce

--- lets-conts.js
+++ lets-conts.js
@@ -7,3 +7,27 @@ console.log(countryName);
 let price= 120;
 price=150;
 console.log(price);
+
+const dress = ['shirt', 'psnt', 'shoe'];
+//dress=['shirt', 'psnt', 'shoe', 'Belt'];ekhane const age array declare korse bole reasign hosse nah kintu push pop kore array object e reasign value array te set kote pari
+dress.push('Belt');
+console.log(dress);
+
+
+//object example
+const student ={
+    name:'Ashik',
+    age:25
+}
+// console.log(student);
+
+//ekhon object value update kora jabe
+student.name='sohel';
+student.age=30;
+console.log(student);
+
+// const student ={
+//     name:'Ashik',
+//     Age:25,
+//     city:'Gazipur'
+// } ekhane object reasign hobe  nah
