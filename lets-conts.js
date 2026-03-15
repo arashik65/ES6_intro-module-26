00840c96388e1c5dbec6864fd01ac5e6cffc9446
arashik65 <mdashikur6398+@gmail.com>
2026-03-16 02:38:02 +0600

    introduce const let in js

---
+++ lets-conts.js
@@ -0,0 +1,9 @@
+//const diye kono variable declare korle sey variable er value no change hobe
+const name= 'Bangladesh';
+// const name ='Singapur';
+const countryName= 'Bd_' + name;
+console.log(countryName);
+//let diye kono variable declare korle sey variable er value change hobe 
+let price= 120;
+price=150;
+console.log(price);
